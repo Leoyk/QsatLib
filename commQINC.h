4f99cbbd97ac2d7db66bb3707efe8453d054b899
Leoyk <gmboxYK@gmail.com>
2018-08-01 15:39:01 +0800

    增加数学函数库并将程序的变量放置在头文件内以增加程序可读性

--- commQINC.h
+++ commQINC.h
@@ -7,6 +7,11 @@
 */
 
 
+
+#include <math.h>
+
+
+
 //5883
 //MPU6050
 #include "commIMU9d.h"
@@ -128,6 +133,95 @@ void getTH(float *t,float *h);
 返回值：浮点型温度值 ℃;
 
 */
+
+
+
+
+
+/**************************变量定义区*****************************************/
+
+////servo	舵机ser
+Servo ser;
+
+
+////SHT31	空气温、湿度
+float airTemp,airHumility;
+
+
+////BMP180	基准气压、当前气压、当前温度、气压高度
+double basePressure,pressure,BmpTem,BmpAltitude;
+
+
+////GPS6M	GPS结构体
+struct commGD gpsData;
+//地面距离（m）	地面距离缓存	高度缓存
+double distance = 0,disbuf = 0,altbuf = 500;
+//初定位时间
+long baseTime;
+
+
+////电池电压
+float batteryVoltage;
+
+
+////数传读取	数传接收完成标志位
+bool UHF_ReadOk = 0;
+String inputString = "";
+
+
+////数传打印	数传输出字符串
+String UHFout = "";
+
+
+////内存卡文件名称	当前编辑的文件名称	
+String fileName = "";
+
+ 
+ 
+////9轴姿态	
+double dt,dgyroXrate,gyroXrate,gyroYrate,declinationAngle;
+//姿态原始数据
+commIMU9d imu9d;
+//姿态结算数据	横滚、俯仰、偏航、指北、全向量和
+double ROLL,PITCH,YAW,HEAD,allAcc;
+//加速度
+#define CONSTANTS_ONE_G 9.8066f    // m/s^2
+//卡尔曼滤波
+Kalman kalmanX; // Create the Kalman instances
+Kalman kalmanY;
+double gyroXangle, gyroYangle; // Angle calculate using the gyro only
+double compAngleX, compAngleY; // Calculated angle using a complementary filter
+double kalAngleX, kalAngleY; // Calculated angle using a Kalman filter
+uint32_t timeDt;
+
+
+
+////运行间隔时间标志
+uint32_t timeMark;
+
+
+
+////数据存储计数
+int cntNum = 0;
+
+
+
+////已释放标志
+int takeFlag = 0;
+////释放信息
+int takeOffInfo = 0;
+
+  int sf;
+  char ck[7] ={'S','h','i','F','a','n','g'};
+
+
+////所有的运行数据
+double data[40];
+
+
+
+////无源蜂鸣器控制引脚
+#define bee  46
 
 
 
